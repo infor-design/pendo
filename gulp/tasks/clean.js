9d35f496ad1c493ab69f264636d43d0a939aaf34
Christopher Lepore <chris.lepore@infor.com>
2017-12-06 14:07:08 -0500

    Add dist files in proper hierarchy

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -10,7 +10,10 @@ module.exports = (gulp, gconfig) => {
 
     return del([
       `${gconfig.paths.site.www}/*.html`,
-      `${gconfig.paths.site.www}/dist/**`
+      `${gconfig.paths.site.www}/dist`,
+      `${gconfig.paths.src.packages}/*/dist/**`,
+      `${gconfig.paths.dist.root}`,
+      `${gconfig.project.zipName}.zip`,
     ]);
   });
 }
